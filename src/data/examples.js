6c1bcb6e42622a34b1413c5e57194d6914980432
VanshikaSaxena123 <vanshika.saxena99@gmail.com>
2025-10-25 20:48:25 +0100

    Added components for examples

--- src/data/examples.js
+++ src/data/examples.js
@@ -1,5 +1,11 @@
 export const examples = [
   {
+    slug: "terrible-form",
+    title: "Terrible Form",
+    img: `${import.meta.env.BASE_URL}images/popup-hell.jpg`,
+    desc: "An absolutely terrible form.",
+  },
+  {
     slug: "popup-hell",
     title: "Popup Hell",
     img: `${import.meta.env.BASE_URL}images/popup-hell.jpg`,

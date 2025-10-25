a170689f9afa94f90fc388828f96be08fb51089b
VanshikaSaxena123 <vanshika.saxena99@gmail.com>
2025-10-25 19:12:39 +0100

    Updated config to run in local dev env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,24 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+// export default defineConfig({
+//   site: "https://vanshikasaxena123.github.io", // your repo URL
+//   base: "/terrible-ui/",
+//   vite: {
+//     plugins: [tailwindcss()]
+//   },
+//   outDir: "./dist",
+// });
+
+// Detect if you're in production (on GitHub Pages)
+const isProd = process.env.NODE_ENV === "production";
+
+// In production, set the base to match your repo name
+// Locally, leave it blank so links and images work
 export default defineConfig({
-  site: "https://vanshikasaxena123.github.io", // your repo URL
-  base: "/terrible-ui/",
+  site: "https://yourusername.github.io/terrible-ui",
+  base: isProd ? "/terrible-ui/" : "/",
   vite: {
     plugins: [tailwindcss()]
   },
-  outDir: "./dist",
 });

4e64115d32bd8162dc0f2c9b2006b57e6cf29e8f
VanshikaSaxena123 <vanshika.saxena99@gmail.com>
2025-10-25 20:21:49 +0100

    implemented slug
    Instead of separate pages for examples

--- src/data/examples.js
+++ src/data/examples.js
@@ -1,8 +1,8 @@
 export const examples = [
   {
+    slug: "popup-hell",
     title: "Popup Hell",
     img: `${import.meta.env.BASE_URL}images/popup-hell.jpg`,
     desc: "Endless popups demanding your attention — the true test of patience.",
-    link: "/examples/popup-hell",
   },
 ];
